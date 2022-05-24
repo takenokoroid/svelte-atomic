f8eeae6d9f4cf66de67fa0f78ed3871d38eca91b
Takaeroid <takenokoroid@gmail.com>
2022-05-24 20:41:25 +0900

    Heder作成

--- src/organisms/Header.svelte
+++ src/organisms/Header.svelte
@@ -3,21 +3,47 @@
   import Icon from "../atoms/Icon.svelte";
   import IconButton from "../atoms/IconButton.svelte";
   import Img from "../atoms/Img.svelte";
-import Text from "../atoms/Text.svelte";
+  import Text from "../atoms/Text.svelte";
   import Search from "../molecules/Search.svelte";
 </script>
 
-<Box>
-  <Icon iconProvider="material-icons" icon="menu" />
-  <Img src="/assets/logo.png" alt="sample"/>
-  <Search />
-  <Icon iconProvider="material-icons" icon="apps" />
-  <Icon iconProvider="material-icons" icon="more_vert" />
-  <IconButton iconProvider="material-icons" icon="account_circle"
-    ><Text class="title">ログイン</Text></IconButton
-  >
+<Box class="header">
+  <Box class="header-content">
+    <Icon iconProvider="material-icons header-icons" icon="menu" />
+    <Img src="/assets/logo.png" alt="sample" class="header-img" />
+  </Box>
+  <Box class="header-content">
+    <Search />
+  </Box>
+  <Box class="header-content">
+    <Icon iconProvider="material-icons header-icons" icon="apps" />
+    <Icon iconProvider="material-icons header-icons" icon="more_vert" />
+    <IconButton iconProvider="material-icons" icon="account_circle">
+      ログイン
+    </IconButton>
+  </Box>
 </Box>
 
 <style>
-
+  :global(.header) {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 12px;
+    border-bottom: solid 1px gray;
+  }
+  :global(.header-img) {
+    width: 120px;
+    height: 56px;
+    object-fit: cover;
+  }
+  :global(.header-content) {
+    display: flex;
+    vertical-align: middle;
+  }
+  :global(.material-icons) {
+    vertical-align: middle;
+  }
+  :global(.header-icons) {
+    padding-top: 15px;
+  }
 </style>
