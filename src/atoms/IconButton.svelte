a8a4a898a375988f1ddf069cb91dddd80706c38c
suzuki.yuto <suzuki.yuto@nifty.co.jp>
2022-05-24 17:59:42 +0900

    途中

--- src/atoms/IconButton.svelte
+++ src/atoms/IconButton.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+import { compute_rest_props } from "svelte/internal";
+
+
   export let iconProvider: string;
   export let icon: string = "";
 </script>
 
 <button>
   <span class={iconProvider}>{icon}</span>
-  <slot><!-- optional fallback --></slot>
+  <slot class={$$props.class}><!-- optional fallback --></slot>
 </button>
 
 <style>
--- src/organisms/Header.svelte
+++ src/organisms/Header.svelte
@@ -3,19 +3,21 @@
   import Icon from "../atoms/Icon.svelte";
   import IconButton from "../atoms/IconButton.svelte";
   import Img from "../atoms/Img.svelte";
+import Text from "../atoms/Text.svelte";
   import Search from "../molecules/Search.svelte";
 </script>
 
 <Box>
   <Icon iconProvider="material-icons" icon="menu" />
-  <Img />
+  <Img src="/assets/logo.png" alt="sample"/>
   <Search />
   <Icon iconProvider="material-icons" icon="apps" />
   <Icon iconProvider="material-icons" icon="more_vert" />
   <IconButton iconProvider="material-icons" icon="account_circle"
-    >ログイン</IconButton
+    ><Text class="title">ログイン</Text></IconButton
   >
 </Box>
 
 <style>
+
 </style>
